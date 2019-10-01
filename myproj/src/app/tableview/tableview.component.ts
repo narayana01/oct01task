7b783c4d2f6a45c536b67d9fc36345cdb4990cd3
narayana jadapally <narayana.jadapally@ojas-it.com>
2019-10-01 16:01:52 +0530

    added timers

--- myproj/src/app/tableview/tableview.component.ts
+++ myproj/src/app/tableview/tableview.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TableserviceService } from '../tableservice.service';
+import { timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators'; 
 
 @Component({
   selector: 'app-tableview',
@@ -14,13 +16,15 @@ data:object;
 idvalue:any;
 na:any;
 emai:any;
+subscription:any;
   
   ngOnInit() {
    this.getemployee();
   }
 getemployee(){
 
-this.ser.getdata().subscribe(res=>{ console.log("employee data",this.data=res);})
+// this.ser.getdata().subscribe(res=>{ console.log("employee data",this.data=res);})
+this.subscription=timer(0,10000).pipe(switchMap(()=>this.ser.getdata())).subscribe(res=>this.data=res)
   
 }
 
